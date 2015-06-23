66d4899b3d2a16137b35b6f038257a7fe916d3e5
Flávio Juvenal <flaviojuvenal@gmail.com>
2015-06-23 12:09:12 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import re
+import os
+import sys
+
+
+name = 'fix-my-django-lib'
+package = 'fixmydjango'
+description = 'A Django app for finding solutions to exceptions'
+url = 'http://github.com/vintasoftware/fix-my-django-lib'
+author = 'Flávio Juvenal da Silva Junior'
+author_email = 'flavio@vinta.com.br'
+license = 'MIT'
+install_requires = [
+    'Django >= 1.8.2',
+]
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+# python setup.py register
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    args = {'version': get_version(package)}
+    print "You probably want to also tag the version now:"
+    print "  git tag -a %(version)s -m 'version %(version)s'" % args
+    print "  git push --tags"
+    sys.exit()
+
+
+setup(
+    name=name,
+    version=get_version(package),
+    url=url,
+    license=license,
+    description=description,
+    author=author,
+    author_email=author_email,
+    packages=get_packages(package),
+    package_data=get_package_data(package),
+    install_requires=install_requires,
+    #test_suite='tox'
+)
