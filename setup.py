828bade69eb279022060beef0baea93cf3287001
Flávio Juvenal <flaviojuvenal@gmail.com>
2016-04-04 01:04:10 -0300

    Bump version to 0.0.11

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ install_requires = [
 
 def get_version(package):
     """
-    Return package version as listed in `__version__` in `init.py`.
+    Return package version as listed in `__version__` in `__init__.py`.
     """
     init_py = open(os.path.join(package, '__init__.py')).read()
     return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
