0bb8a4af0a369d49b45888dbab000f80d575bb94
anderson <anderson@vinta.com.br>
2016-11-10 15:29:57 -0300

    Updated error creation url

--- fixmydjango/client.py
+++ fixmydjango/client.py
@@ -5,14 +5,15 @@ from django.conf import settings
 base_url = getattr(
     settings,
     'FIX_MY_DJANGO_API_BASE_URL',
-    'http://www.fixmydjango.com')
+    'http://www.fixmydjango.com'
+)
 
 
 def search_exceptions(exception_type, raised_by, raised_by_line='',
                       django_version='', base_url=base_url):
-    path = '/api/search/'
-
-    return requests.get(base_url + path, params={
+    search_path = '/api/search/'
+    url = '{0}{1}'.format(base_url, search_path)
+    return requests.get(url, params={
         'exception_type': exception_type,
         'raised_by': raised_by,
         'raised_by_line': raised_by_line,
